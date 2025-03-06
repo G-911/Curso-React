df82516e76b10f6b211f8310d437c555ed5e816b
G-911 <g.avila-2002@hotmail.com>
2025-03-06 13:27:41 -0400

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import './style/App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Duran',
+        isFollowing: true,
+    },
+    {
+        userName: 'g-911',
+        name: 'Gabriel Avila',
+        isFollowing: true,
+    },
+    {
+        userName: 'pheralb',
+        name: 'Paco Hernandez',
+        isFollowing: false,
+    }
+]
+    
 export function App () {
     return (
         <section className='App'>
-                <TwitterFollowCard 
-                    initialIsFollowing={true}
-                    userName="midudev">
-                        Miguel Duran
-                </TwitterFollowCard>
-
-                <TwitterFollowCard 
-                    userName="G-911"> 
-                    Gabriel Avila
-                </TwitterFollowCard>
-
-                <TwitterFollowCard 
-                    userName="elonmusk"> 
-                    Elon Musk
-                </TwitterFollowCard>
-
-                <TwitterFollowCard  
-                    userName="vxnder"> 
-                    Vanderhart
-                </TwitterFollowCard>
+            {
+                users.map(user => {
+                    const{ userName, name, isFollowing} = user
+                    return(
+                        <TwitterFollowCard
+                        key={userName}
+                        userName = {userName}
+                        initialIsFollowing = {isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
         </section>
     )
 }
